ece27a7a21331b05becde27c63d1d6f64d83088b
BimbaMan <ivan.d.sidorenko@gmail.com>
2023-01-20 11:16:37 +0300

    add Enter key handler

--- todos-app/src/App.tsx
+++ todos-app/src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { KeyboardEvent, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+("use strict");
 
 function RenderApp() {
-  
-  return(
+  const keyup = (event: KeyboardEvent): void => {
+    //console.log(event);
+
+    if (event.key !== "Enter") {
+      return;
+    }
+
+    event.preventDefault();
+
+    if (event.key === "Enter") {
+      console.log("Test Message");
+    }
+  };
+
+  return (
     <header className="header">
       <h1>TODOs</h1>
-      <input className="new-todo" placeholder="What needs to be done?"></input>
+      <input
+        onKeyUp={keyup}
+        className="new-todo"
+        placeholder="What needs to be done?"
+      ></input>
     </header>
-  )
+  );
 }
 
-export default RenderApp
+export default RenderApp;
--- todos-app/src/main.tsx
+++ todos-app/src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import RenderApp from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RenderApp from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RenderApp/>
-  </React.StrictMode>,
-)
+    <RenderApp />
+  </React.StrictMode>
+);
