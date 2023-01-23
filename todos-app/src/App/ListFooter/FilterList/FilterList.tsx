006e6e5fe59e4e02795c6973df6868b05a6b9eb4
BimbaMan <ivan.d.sidorenko@gmail.com>
2023-01-23 19:05:13 +0300

    add filter buttons

--- todos-app/src/App/ListFooter/FilterList/FilterList.tsx
+++ todos-app/src/App/ListFooter/FilterList/FilterList.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "../../../App.css";
 
 const FilterList: React.FC = () => {
-  return <div></div>;
+  return (
+    <ul className="filters">
+      <li>
+        <a className="selected">All</a>
+      </li>{" "}
+      <li>
+        <a className="">Active</a>
+      </li>{" "}
+      <li>
+        <a className="">Completed</a>
+      </li>
+    </ul>
+  );
 };
+
+/* i'll need it later*/
+//
+// const FilterList: React.FC = () => {
+//   const { pathname } = useLocation();
+//   return (
+//     <ul className="filters">
+//       <li>
+//         <a className={pathname === "/" ? "selected" : ""} href="/">
+//           All
+//         </a>
+//       </li>{" "}
+//       <li>
+//         <a className={pathname === "/active" ? "selected" : ""} href="/active">
+//           Active
+//         </a>
+//       </li>{" "}
+//       <li>
+//         <a
+//           className={pathname === "/completed" ? "selected" : ""}
+//           href="/completed"
+//         >
+//           Completed
+//         </a>
+//       </li>
+//     </ul>
+//   );
+// };
 
 export default FilterList;
